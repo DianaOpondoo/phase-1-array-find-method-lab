9891b06acdf528129e832658f9582e946fa9339f
Diana Awino Opondo <dianaawino1@gmail.com>
2022-06-14 03:58:00 +0300

    commit tree ca5bea603d4ece62262e5bf30d4a942b97abc58d

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+]
 // code your solution here
+superbowlWin = (record) => {
+    const result = record.find( record => record.result === "W" );
+    return !!result ? result.year : undefined;
+  };
+
+record.find( record => record.result === "W" ).year;
